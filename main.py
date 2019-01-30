1fa83836ddf2c4a89fa62a9e6b5f8cb85e65fb63
salari <salari.m1375@gmail.com>
2019-01-30 17:38:13 +0330

    Add preparing pokemon-spawns data

--- main.py
+++ main.py
@@ -1,6 +1,93 @@
 import numpy as np
+import pandas as pd
 import itertools
 
+
+class Grid:
+    def __init__(self, name, min_x, max_x, min_y, max_y, last_x, last_y):
+        self.min_x = min_x
+        self.max_x = max_x
+        self.min_y = min_y
+        self.max_y = max_y
+        self.last_x = last_x
+        self.last_y = last_y
+        self.name = name
+        self.points = list()
+        self.freq = 0
+        self.prob = 0.0
+        
+    def get_center(self):
+        if self.freq == 0:
+            return [(self.min_x+self.max_x)/2, (self.min_y+self.max_y)/2]
+        return [sum(i)/len(self.points) for i in zip(*self.points)]
+    
+    def is_in(self, lat, lng):
+        if lat < self.min_x or not self.last_x and lat >= self.max_x or self.last_x and lat > self.max_x:
+            return False
+        if lng < self.min_y or not self.last_y and lng >= self.max_y or self.last_y and lng > self.max_y:
+            return False
+        return True
+
+def convert_pokemon_data(path, n, t):
+    data = pd.read_csv(path)[['name', 'lat', 'lng']]
+    names = pd.unique(data[['name']].values.ravel('K'))[:n]
+    data = data[data['name'].isin(names)]
+    min_lat, max_lat, min_lng, max_lng = min(data['lat']), max(data['lat']), min(data['lng']), max(data['lng'])
+    
+    lats = []
+    for i in range(t):
+        lats.append(min_lat + ((max_lat-min_lat)/t)*i)
+    lats.append(max_lat)
+    lngs = []
+    for i in range(t):
+        lngs.append(min_lng + ((max_lng-min_lng)/t)*i)
+    lngs.append(max_lng)
+    
+    grids = {}
+    for name in names:
+        grids[name] = []
+        for i in range(len(lats)-1):
+            for j in range(len(lngs)-1):
+                last_x = (i == len(lats)-2)
+                last_y = (j == len(lngs)-2)
+                grids[name].append(Grid(name, lats[i], lats[i+1], lngs[j], lngs[j+1], last_x, last_y))
+                
+    cnt = data.groupby('name').size()
+    for _, row in data.iterrows():
+        for grid in grids[row['name']]:
+            if grid.is_in(row['lat'], row['lng']):
+                grid.freq += 1
+                grid.points.append([row['lat'], row['lng']])
+                break
+    
+    for name in grids.keys():
+        for grid in grids[name]:
+            grid.prob = (float)(grid.freq) / cnt[name]
+            
+    net_probs = np.zeros((n, t*t))
+    network = np.zeros((n, t*t, 2))
+    for i in range(n):
+        name = names[i]
+        for j in range(t*t):
+            grid = grids[name][j]
+            net_probs[i][j] = grid.prob
+            center = grid.get_center()
+            network[i][j][0] = center[0]
+            network[i][j][1] = center[1]
+    
+    return net_probs, network
+        
+    def get_center(self):
+        if self.freq == 0:
+            return [(self.min_x+self.max_x)/2, (self.min_y+self.max_y)/2]
+        return [sum(i)/len(self.points) for i in zip(*self.points)]
+    
+    def is_in(self, lat, lng):
+        if lat < self.min_x or not self.last_x and lat >= self.max_x or self.last_x and lat > self.max_x:
+            return False
+        if lng < self.min_y or not self.last_y and lng >= self.max_y or self.last_y and lng > self.max_y:
+            return False
+        return True
 
 def get_ecost(net_probs, network, min_centers):
     n = network.shape[0]
@@ -73,18 +160,31 @@ def approx_kcenter(net_probs, network, k):
         centers = np.vstack([centers, new_center])
     return centers
 
+def test(net_probs, network, k):
+    exact_center = exact_kcenter(net_probs, network, k)
+    approx_center = approx_kcenter(net_probs, network, k)
+    n = network.shape[0]
+
+    exact_min_center = np.zeros((n,2))
+    for i in range(n):
+        exact_min_center[i], _ = find_center(net_probs[i], network[i], exact_center)
+    approx_min_center = np.zeros((n,2))
+    for i in range(n):
+        approx_min_center[i], _ = find_center(net_probs[i], network[i], approx_center)
+
+    exact_cost = get_ecost(net_probs, network, exact_min_center)
+    approx_cost = get_ecost(net_probs, network, approx_min_center)
+
+    return exact_center, approx_center, exact_cost, approx_cost
+
+
 def run():
-    net_prob = np.array([[0.25, 0.25, 0.25, 0.25], [0.25, 0.25, 0.25, 0.25]])
-    network = np.array([[[0,0], [1,1], [2,2], [3,3]], [[1,1], [2,2], [3,3], [4,4]]])
+    # net_probs = np.array([[0.5, 0.5], [0.5, 0.5], [0.5, 0.5]])
+    # network = np.array([[[0,0], [3,3]], [[2,1], [1,3]], [[3,1], [0,3]]])
 
-    c1 = exact_kcenter(net_prob, network, 2)
-    c2 = approx_kcenter(net_prob, network, 2)
+    net_probs, network = convert_pokemon_data('pokemon-spawns.csv', 2, 2)
 
-    print(c1)
-    print(c2)
-
-    print(get_ecost(net_prob, network, c1))
-    print(get_ecost(net_prob, network, c2))
+    print(test(net_probs, network, 2))
 
 if __name__ == '__main__':
     run()
